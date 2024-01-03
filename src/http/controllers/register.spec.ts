4cf67779a70c89ab8c8249e7a5e49761b511a777
Danrlei <danrleidalfre@gmail.com>
2024-01-03 15:23:45 -0300

    implement tests controllers auth, profile and register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
 
-test('ok', () => {
-  console.log('test')
+describe('Register Controller e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'User',
+      email: 'user@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
